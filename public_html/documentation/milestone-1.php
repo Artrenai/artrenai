d396245fab9e3358c85c1c30c53c517a18134660
Godfrey Davis <gdavis42@cnm.edu>
2017-07-20 03:26:26 -0600

    the initial commit no coding just yet just all filenames an directories in palce

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,8 +6,28 @@
 			</title>
 		</head>
 		<body>
-			<h2>
+			<h1>
 				Milestone One
-			</h2>
+			</h1>
+			<h2>Purpose, Audience, Goal</h2>
+			<p>
+				<ul>
+				<li>
+					<strong>Purpose:</strong>
+					To display my work and attract many different types of employers outside the scope of what my portfolio
+					showcases.
+				</li>
+				<li>
+					<strong>Audience:</strong>
+
+				</li>
+
+			</ul>
+
+
+			</p>
+			<h2>Persona</h2>
+			<h2>Use Case</h2>
+
 		</body>
 </html>
