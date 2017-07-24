9cf746d89870dfc07462d60f5c2a965b5cb093c1
Godfrey Davis <gdavis42@cnm.edu>
2017-07-23 21:19:14 -0600

    finished pwp milestone 1 -fixed ul indents deleted some text from audience to limit scope

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -10,59 +10,24 @@
 				Milestone One
 			</h1>
 			<h2>Purpose, Audience, Goal</h2>
-			<p>
-				<ul>
-				<li>
-					<strong>Purpose:</strong>
-					To display my work and attract many different types of employers outside the scope of what my portfolio
-					showcases.
-				</li>
-				<li>
-					<strong>Audience:</strong>
-					People who need portriaits,headshots and candid professional photos. Resturants for Menu items. Retail for store Items
-					hair salons for portfolio and website gallery updates. Movie producers and event promoters for photography and photmanipulation
-					for print marketing. Small businesses. Clubs and Event orginizers.
-				</li>
-				<li>
-					<strong>Goal:</strong>
-					I want to restart my photography and photo editing/manipulation portfolio. Transition from a person who just does this as a hobby
-				to a professional who freelances to suppliment income while going to school. I also want to work outside my comfort zone.
-				</li>
+			<ul>
+				<li><strong>Purpose:</strong>To display my work and attract many different types of employers outside the scope of what my portfolio
+				showcases.</li>
+				<li><strong>Audience:</strong>People who need portriaits,headshots and candid professional photos, Photography and photmanipulation
+				for print marketing and Small businesses.</li>
+				<li><strong>Goal:</strong>I want to restart my photography and photo editing/manipulation portfolio. Transition from a person who just does this as a hobby
+				to a professional who freelances to suppliment income while going to school. I also want to work outside my comfort zone.</li>
 			</ul>
-
-
-			</p>
 			<h2>Persona</h2>
-
-			<p>
-				<ul>
-				<li>
-				</li>
-				<li
-				</li>
-				<li>
-					<strong>Role/Audience:</strong>
-					Future Employers, Small businesses, Anyone looking for an experienced  photographer.
-				</li>
-				<li>
-					<strong>Technology:</strong>
-					Computer, Internet SmartPhone
-				</li>
-				<li>
-					<strong>Goal:</strong>
-					Attract a broader scope of clients. Have consistent work.
-				</li>
-				<li>
-					<strong>Frustrations:</strong>
-					It has been a struggle getting the clientel, the current portfolio is limited. So, it will be harder to advertise
-					myself into markets i havent worked.
-				</li>
-
-				</ul>
-			</p>
+			<ul>
+				<li><strong>Role/Audience:</strong>Future Employers, Small businesses, Anyone looking for an experienced  photographer.</li>
+				<li><strong>Technology:</strong>Computer, Internet SmartPhone</li>
+				<li><strong>Goal:</strong>The website is convincing enough to hire Godfrey for work.</li>
+				<li><strong>Frustrations:</strong>the website is very limited. as it is now...Not enough evidence that he has any photography experince outside fashion photography</li>
+			</ul>
 			<h2>Use Case</h2>
 			<p>
-			I want to reach a broader audience. My photography and art has been at a hault for a while now.Im looking to build a
+				I want to reach a broader audience. My photography and art has been at a hault for a while now.Im looking to build a
 				website to reintroduce me into the work and showcase the additional web-developer skills i have aquired. I also want to
 				make sure that this website shows that I have a grasp on UX/UI design.
 			</p>
