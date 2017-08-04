c1957fecd070ac9c2be3350e8f79a9a8ce85f210
Godfrey Davis <gdavis42@cnm.edu>
2017-08-04 09:28:11 -0600

    milestone php 2 added

---
+++ public_html/documentation/mileston-2.php
@@ -0,0 +1,31 @@
+<<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>
+			Artrenai.com
+		</title>
+	</head>
+	<body>
+		<h1>
+			Milestone Two
+		</h1>
+		<h2>Content Strategy</h2>
+		<p>The essentials will have to be the header and footer. In the header will be the navigation menu which will consist of </p>
+			<ul>
+			<li>Bio</li>
+			<li>Portfiolio
+				<ul>
+					<li>Photography</li>
+					<li>Edits and Manipulation</li>
+					<li>Experimental</li>
+				</ul>
+			</li>
+			<li>Contact</li>
+		</ul>
+			<p>Allot of the website will be graphical as possible. Its important that the page palpable.thinking mobile first
+		 	this has to translate well on a smaller screen. I want my images to scale and allot of the interactive functionally
+				will have to be disabled but i would like to get away with as much as possible . on desktop i want to use a modal for contact
+			but for mobile this will be featured at the bottom of the page. The footer will consist of copyright jargon and links to social network</p>
+
+			<img src="image/wireframes.png" />
